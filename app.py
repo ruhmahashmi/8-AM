abefeee78767034bfe8b2d18f6e00360727cd9d3
gy92 <gy92@drexel.edu>
2025-04-30 11:51:19 -0400

    fixes

--- app.py
+++ app.py
@@ -44,7 +44,6 @@ class Course(db.Model):
     start_time = db.Column(db.String(10), nullable=False)
     end_time = db.Column(db.String(10), nullable=False)
     day = db.Column(db.String(10), nullable=False)
-    credits = db.Column(db.Integer, nullable=False, default=3)
 
 class Schedule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
